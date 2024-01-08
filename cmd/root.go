3db9fac87dfaea882ec5ccff53444a4cb1c782ed
Marek Gajewski <marek.gajewski2001@gmail.com>
2024-01-08 13:37:01 +0100

    root organisation

--- cmd/root.go
+++ cmd/root.go
@@ -1,172 +1,148 @@
 package cmd
 
 import (
-	"fmt"
-	"math"
-	"os"
-	"os/exec"
-	"time"
+    "fmt"
+    "os"
+    "os/exec"
+    "time"
 
-	pkg "github.com/cmgriffing/will-it-blend/pkg"
-	"github.com/google/shlex"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+    "github.com/cmgriffing/will-it-blend/pkg"
+    "github.com/google/shlex"
+    "github.com/mitchellh/go-homedir"
+    "github.com/spf13/cobra"
+    "github.com/spf13/viper"
 )
 
-var cfgFile = ""
-var title = ""
-var duration = 30
-var successMessage = ""
-var failureMessage = ""
-var port = pkg.AllowedPort3000
-var token = ""
+// Config represents the configuration settings
+type Config struct {
+    File           string
+    Title          string
+    Duration       int
+    SuccessMessage string
+    FailureMessage string
+    Port           pkg.AllowedPort
+    Token          string
+}
+
+var cfg Config
 
 var RootCmd = &cobra.Command{
-	Use:     "will-it-blend",
-	Version: "0.3.2",
-	Short:   "will-it-blend is a tool for automating the creation of CLI command based Twitch.tv predictions",
-	Long: `will-it-blend is a tool for automating the creation of CLI command based Twitch.tv predictions.
-
-It will resolve the prediction based on the return code of the specified command.`,
-	Example: "will-it-blend \"ls -a\"",
-	Args:    cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		command := args[0]
-		// fmt.Println("cfgFile", cfgFile)
-		// fmt.Println("command in rootCmd", command)
-		// fmt.Println("title", title)
-		// fmt.Println("duration", duration)
-		// fmt.Println("successMessage", successMessage)
-		// fmt.Println("failureMessage", failureMessage)
-		// fmt.Println("port", port)
-		// fmt.Println("token", token)
-
-		if token == "" {
-			token = pkg.StartServer(port)
-		}
-
-		userId := pkg.GetUserId(token)
-
-		prediction := pkg.CreatePrediction(token, title, userId, successMessage, failureMessage, duration)
-
-		fmt.Printf("Prediction running. Command will run after %d seconds \n", duration)
-
-		time.Sleep(time.Duration(duration) * time.Second)
-
-		loopCount := 0
-		loopSleep := 5
-		loopMax := 90 / loopSleep
-		finished := false
-
-		for loopCount < loopMax {
-			time.Sleep(time.Duration(loopSleep) * time.Second)
-			finished = pkg.IsPredictionFinished(token, userId, prediction.PredictionId)
-			loopCount = loopCount + 1
-			if finished {
-				loopCount = math.MaxInt
-			}
-		}
-
-		if !finished {
-			fmt.Println("Prediction did not lock as expected")
-			os.Exit(1)
-		}
-
-		commandSuccess := runCommand(command)
-
-		winningOutcomeId := prediction.SuccessOutcomeId
-		if !commandSuccess {
-			winningOutcomeId = prediction.FailureOutcomeId
-		}
-
-		pkg.ResolvePrediction(token, userId, prediction.PredictionId, winningOutcomeId)
-
-		fmt.Println("Prediction Resolved")
-
-	},
+    Use:     "will-it-blend",
+    Version: "0.3.2",
+    Short:   "will-it-blend is a tool for automating the creation of CLI command based Twitch.tv predictions",
+    Long: `will-it-blend is a tool for automating the creation of CLI command based Twitch.tv predictions.
+           It will resolve the prediction based on the return code of the specified command.`,
+    Example: "will-it-blend \"ls -a\"",
+    Args:    cobra.ExactArgs(1),
+    Run:     runRootCmd,
 }
 
-func Init() {
+func runRootCmd(cmd *cobra.Command, args []string) {
+    command := args[0]
 
-	RootCmd.Flags().SortFlags = false
-	RootCmd.PersistentFlags().SortFlags = false
+    // Token handling
+    if cfg.Token == "" {
+        cfg.Token = pkg.StartServer(cfg.Port)
+    }
 
-	RootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "~/.config/.will-it-blend.yaml", "config file (default is $HOME/.config/.will-it-blend.yaml)")
-	RootCmd.PersistentFlags().StringVarP(&title, "title", "t", "Will it blend?", "title of prediction (default is \"Will it blend?\")")
-	RootCmd.PersistentFlags().IntVarP(&duration, "duration", "d", 60, "the duration of the prediction timer (default is 60 seconds)")
-	RootCmd.PersistentFlags().StringVarP(&successMessage, "success", "s", "Yes", "success message of prediction (default is \"Yes\")")
-	RootCmd.PersistentFlags().StringVarP(&failureMessage, "failure", "f", "No", "failure message of prediction (default is \"No\")")
+    userId := pkg.GetUserId(cfg.Token)
+    prediction := pkg.CreatePrediction(cfg.Token, cfg.Title, userId, cfg.SuccessMessage, cfg.FailureMessage, cfg.Duration)
 
-	RootCmd.PersistentFlags().StringVarP(&token, "token", "k", "", "auth token for Twitch")
+    fmt.Printf("Prediction running. Command will run after %d seconds\n", cfg.Duration)
+    time.Sleep(time.Duration(cfg.Duration) * time.Second)
 
-	RootCmd.Flags().VarP(&port, "port", "p", `auth server port. allowed: 1337, 3000, 4242, 6666, 6969, 8000, 8008, 8080, 42069`)
+    loopCount := 0
+    loopSleep := 5
+    loopMax := 90 / loopSleep
+    finished := false
 
-	cobra.OnInitialize(initConfig)
+    for loopCount < loopMax && !finished {
+        time.Sleep(time.Duration(loopSleep) * time.Second)
+        finished = pkg.IsPredictionFinished(cfg.Token, userId, prediction.PredictionId)
+        loopCount++
+    }
 
-	viper.BindPFlag("title", RootCmd.PersistentFlags().Lookup("title"))
-	// allSettings := viper.AllSettings()
-	// if allSettings != nil {
-	// 	fmt.Println("allSettings", allSettings)
-	// 	fmt.Println("title", viper.GetString("title"))
-	// }
+    if !finished {
+        fmt.Println("Prediction did not lock as expected")
+        return // Return error in real-world usage
+    }
+
+    commandSuccess := runCommand(command)
+    winningOutcomeId := getWinningOutcomeId(prediction, commandSuccess)
+    pkg.ResolvePrediction(cfg.Token, userId, prediction.PredictionId, winningOutcomeId)
+
+    fmt.Println("Prediction Resolved")
 }
 
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-		viper.WriteConfig()
-	} else {
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println("Failed to find homedir", err)
-			os.Exit(1)
-		}
-
-		configDir := fmt.Sprintf("%s/.config", home)
-
-		configDirExists, err := os.Stat(configDir)
-		if err != nil && !os.IsNotExist(err) {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		if !configDirExists.IsDir() {
-			fmt.Println("HOME/.config exists but is not a directory")
-			os.Exit(1)
-		}
-
-		viper.AddConfigPath(configDir)
-		viper.SetConfigName(".will-it-blend")
-	}
-
+func getWinningOutcomeId(prediction pkg.Prediction, commandSuccess bool) string {
+    if commandSuccess {
+        return prediction.SuccessOutcomeId
+    }
+    return prediction.FailureOutcomeId
 }
 
 func runCommand(command string) bool {
-	parts, err := shlex.Split(command)
+    parts, err := shlex.Split(command)
+    if err != nil {
+        fmt.Println("Failed to parse command:", err)
+        return false
+    }
 
-	if err != nil {
-		fmt.Println("Failed to parse command", err)
-		os.Exit(1)
-	}
+    cmd := exec.Command(parts[0], parts[1:]...)
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
 
-	cmd := exec.Command(parts[0], parts[1:]...)
+    if err := cmd.Start(); err != nil {
+        fmt.Println("Failed to start command:", err)
+        return false
+    }
 
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+    if err := cmd.Wait(); err != nil {
+        return false
+    }
+    return true
+}
 
-	err = cmd.Start()
+func Init() {
+    RootCmd.PersistentFlags().StringVarP(&cfg.File, "config", "c", "~/.config/.will-it-blend.yaml", "config file (default is $HOME/.config/.will-it-blend.yaml)")
+    RootCmd.PersistentFlags().StringVarP(&cfg.Title, "title", "t", "Will it blend?", "title of prediction (default is \"Will it blend?\")")
+    RootCmd.PersistentFlags().IntVarP(&cfg.Duration, "duration", "d", 60, "the duration of the prediction timer (default is 60 seconds)")
+    RootCmd.PersistentFlags().StringVarP(&cfg.SuccessMessage, "success", "s", "Yes", "success message of prediction (default is \"Yes\")")
+    RootCmd.PersistentFlags().StringVarP(&cfg.FailureMessage, "failure", "f", "No", "failure message of prediction (default is \"No\")")
+    RootCmd.PersistentFlags().StringVarP(&cfg.Token, "token", "k", "", "auth token for Twitch")
+    RootCmd.Flags().VarP(&cfg.Port, "port", "p", `auth server port. allowed: 1337, 3000, 4242, 6666, 6969, 8000, 8008, 8080, 42069`)
 
-	if err != nil {
-		fmt.Println("Failed to start command")
-		os.Exit(1)
-	}
+    cobra.OnInitialize(initConfig)
+}
 
-	err = cmd.Wait()
+func initConfig() {
+    if cfg.File != "" {
+        viper.SetConfigFile(cfg.File)
+        viper.WriteConfig()
+    } else {
+        home, err := homedir.Dir()
+        if err != nil {
+            fmt.Println("Failed to find homedir", err)
+            return
+        }
 
-	if err != nil {
-		return false
-	} else {
-		return true
-	}
+        configDir := fmt.Sprintf("%s/.config", home)
+
+        configDirExists, err := os.Stat(configDir)
+        if os.IsNotExist(err) {
+            fmt.Println("HOME/.config does not exist")
+            return
+        }
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        if !configDirExists.IsDir() {
+            fmt.Println("HOME/.config exists but is not a directory")
+            return
+        }
+
+        viper.AddConfigPath(configDir)
+        viper.SetConfigName(".will-it-blend")
+    }
 }
